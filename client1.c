df0ea844ef5d11971e64b6416a4c8450fa5eeb5d
QuinnMcHenry <quinn.mchenry@student.chaminade.edu>
2026-03-21 21:46:01 -0800

    fix

--- client1.c
+++ client1.c
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Socket creation error \n");
+        printf("Socket creation error\n");
         return -1;
     }
 
@@ -25,57 +25,44 @@ int main(int argc, char const *argv[])
 
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
-        printf("\nInvalid address/ Address not supported \n");
+        printf("Invalid address/ Address not supported\n");
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("\nConnection Failed \n");
+        printf("Connection Failed\n");
         return -1;
     }
 
-    // send our PID to the server so it knows which client we are
-    pid_t pid = getpid();
-    send(sock, &pid, sizeof(pid_t), 0);
-    printf("Client 1 connected with PID: %d\n", pid);
-
-    // wait to hear if the server has both clients connected
+    // read the server's initial status message and just print it
+    // server sends "only one client is up" right away - we just acknowledge it
     memset(buffer, 0, 1024);
     valread = read(sock, buffer, 1024);
-    printf("Server: %s\n", buffer);
+    printf("Server says: %s\n", buffer);
 
-    // if server says only one client is up, exit
-    if (strncmp(buffer, "only one client is up", 21) == 0)
-    {
-        close(sock);
-        return -1;
-    }
-
-    // main loop - keep sending and receiving until we type BYE
+    // main loop
     while (1)
     {
-        // read message from user with scanf
-        printf("Client 1 - Enter message: ");
-        scanf("%s", message);
+        // client 1 always sends first
+        printf("Enter message: ");
+        fgets(message, 1024, stdin);
+        message[strcspn(message, "\n")] = 0;
 
-        // send message to server
         send(sock, message, strlen(message), 0);
 
-        // if we said BYE, stop
-        if (strcmp(message, "BYE") == 0)
+        if (strncmp(message, "BYE", 3) == 0)
         {
             printf("Disconnecting...\n");
             break;
         }
 
-        // wait for the forwarded message from client 2 via server
+        // wait for client 2's response forwarded by the server
         memset(buffer, 0, 1024);
         valread = read(sock, buffer, 1024);
         printf("Client 2 says: %s\n", buffer);
 
-        // if client 2 said BYE, stop
-        if (strcmp(buffer, "BYE") == 0)
+        if (strncmp(buffer, "BYE", 3) == 0)
         {
             printf("Client 2 has disconnected. Exiting...\n");
             break;
